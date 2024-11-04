bbe7a677b2fd5866e6e5cd3f4ae92ce9e02cb67d
Marsani <marsanix@gmail.com>
2024-11-04 14:57:40 +0700

    Initial commit

---
+++ app/src/main/java/com/codecrafterkw/smartclassjurnalkw/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.codecrafterkw.smartclassjurnalkw
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.codecrafterkw.smartclassjurnalkw.ui.theme.SmartClassJurnalKwTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SmartClassJurnalKwTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SmartClassJurnalKwTheme {
+        Greeting("Android")
+    }
+}
